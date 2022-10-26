d1f51d05eaf40ba763dab7e5e7cb76b10ed0ae13
Muhammad Risqi Firdaus <risqi.firdaus20@gmail.com>
2022-10-26 12:52:40 +0700

    feat: add album pages

---
+++ src/api/admin/add-album.php
@@ -0,0 +1,69 @@
+<?php
+    include "../connect.php";
+
+
+    $judul = $_POST["title"];
+    $penyanyi = $_POST['singer'];
+    $genre = $_POST['genre'];
+    $tanggal_terbit = $_POST['tanggalTerbit'];
+
+    $tartgetDir = __DIR__.'/../../assets/';
+    $baseFileName = time() . "-" ;
+
+    //image path
+    $tempName = $baseFileName . basename($_FILES["f_image"]["name"]);
+    $targetImgPath = $tartgetDir . 'images/' . $tempName;
+    $fileImgType = strtolower(pathinfo($targetImgPath,PATHINFO_EXTENSION));
+    $imgTypesAllowed = array("jpg","jpeg", "png");
+
+    //cek tipe image
+    if(($_FILES["f_image"]["size"]) <=0|| !in_array($fileImgType,$imgTypesAllowed)){
+        http_response_code(400);
+        exit(json_encode(
+            [
+                "status" => 400,
+                "message" => "File bukan gambar atau tipe gambar tidak sesuai",
+                "data" => $_FILES["f_image"]["size"]
+            ]
+        ));
+    }
+
+    //move upload file
+    if(!move_uploaded_file($_FILES["f_image"]["tmp_name"], $targetImgPath)){
+        http_response_code(500);
+        exit(json_encode(
+            [
+                "status" => 500,
+                "message" => "Gagal upload gambar",
+                "data" => ""
+            ]
+        ));
+    }
+
+    $imgPath = '/assets/images/' . $tempName;
+
+    $query = "INSERT INTO album (judul, penyanyi, image_path, genre, tanggal_terbit, total_duration) 
+            VALUES ('$judul', '$penyanyi', '$imgPath', '$genre', '$tanggal_terbit', 0);";
+    
+    
+
+    if(!$conn->query($query)){
+        http_response_code(501);
+        exit(json_encode(
+            [
+                "status" => 501,
+                "message" => "Error in adding album, please try again later!",
+                "data" => $query
+            ]
+        ));
+    }
+    
+    http_response_code(200);
+    exit(json_encode(
+        [
+            "status" => 200,
+            "message" => "Berhasil menambahkan album",
+            "data" => ""
+        ]
+    ));
+?>
